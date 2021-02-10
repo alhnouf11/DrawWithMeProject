3ccda2aeabba26f55213f1b196b3d5072761b794
Arwa alquhaiz أروى القحيز <60386106+ArwaData@users.noreply.github.com>
2021-02-10 22:12:36 +0300

    drawing page

--- DrawWithMe/colorViewController.swift
+++ DrawWithMe/colorViewController.swift
@@ -6,24 +6,25 @@
 //
 
 import UIKit
-
+import PencilKit
 class colorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        setUpDrawing()
     }
-    */
+    
+    func setUpDrawing() {
+        let canvasView = PKCanvasView(frame:  self.view.bounds)
+        guard let window = view.window, let toolPicker = PKToolPicker.shared(for: window) else {return}
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+        view.addSubview(canvasView)
+    }
 
 }
--- DrawWithMe/tracingViewController.swift
+++ DrawWithMe/tracingViewController.swift
@@ -6,16 +6,71 @@
 //
 
 import UIKit
+import PencilKit
 
 class tracingViewController: UIViewController {
 
+    @IBOutlet weak var canvasView: UIView!
+    
+    var path = UIBezierPath()
+    var startPoint = CGPoint()
+    var touchPoint = CGPoint()
+    
+    
+    @IBAction func eraserClear(_ sender: UIButton) {
+        
+        path.removeAllPoints()
+        canvasView.layer.sublayers = nil
+        canvasView.setNeedsDisplay()
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        canvasView.clipsToBounds = true
+        canvasView.isMultipleTouchEnabled = false
+        
 
-        // Do any additional setup after loading the view.
     }
     
 
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let point = touch?.location(in: canvasView){
+            startPoint = point
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let point = touch?.location(in: canvasView){
+            touchPoint = point
+        }
+        
+        path.move(to: startPoint)
+        path.addLine(to: touchPoint)
+        startPoint = touchPoint
+        //call draw
+        draw()
+    }
+    
+    func draw(){
+        let strokeLayer = CAShapeLayer()
+        strokeLayer.fillColor = nil
+        strokeLayer.lineWidth = 5
+        strokeLayer.strokeColor = UIColor.red.cgColor
+        strokeLayer.path = path.cgPath
+        canvasView.layer.addSublayer(strokeLayer)
+        canvasView.setNeedsDisplay()
+    }
+    
+    @IBAction func eraser1(_ sender: UIButton) {
+        
+        
+    }
+    
+    
     /*
     // MARK: - Navigation
 
