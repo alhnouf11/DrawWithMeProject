5bd03a41110ad80ea29da5f4e754782edc3a24a8
Arwa alquhaiz أروى القحيز <60386106+ArwaData@users.noreply.github.com>
2021-02-16 23:29:58 +0300

    upload image

--- DrawWithMe/addViewController.swift
+++ DrawWithMe/addViewController.swift
@@ -8,28 +8,44 @@
 import UIKit
 
 class addViewController: UIViewController {
-
+    @IBOutlet var imgView:UIImageView!
     @IBAction func backButton(_ sender: Any) {
         let vcAddUI = self.storyboard?.instantiateViewController(identifier: "switchUI")
         navigationController?.pushViewController(vcAddUI!, animated: true)
-        
-        
     }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTabButton() {
+        let vc =  UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+        
+        
+        
     }
-    */
 
+ 
+}
+
+extension addViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+        
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")]as? UIImage{
+            imgView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
--- DrawWithMe/profileViewController.swift
+++ DrawWithMe/profileViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class profileViewController: UIViewController {
-
+    @IBOutlet var imgView:UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTabButton() {
+        let vc =  UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+        
+        
+        
     }
-    */
 
 }
+
+extension profileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+        
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")]as? UIImage{
+            imgView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
