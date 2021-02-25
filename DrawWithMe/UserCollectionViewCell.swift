ab2b2210ca18fd2a896e59a0b5922ec57a2abd27
alhnouf11 <78360466+alhnouf11@users.noreply.github.com>
2021-02-26 01:22:24 +0300

    switch

---
+++ DrawWithMe/UserCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserCollectionViewCell.swift
+//  DrawWithMe
+//
+//  Created by Alhnouf F on 14/07/1442 AH.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imgView : UIImageView!
+    @IBOutlet weak var nameLabel : UILabel!
+}
--- DrawWithMe/ViewController.swift
+++ DrawWithMe/ViewController.swift
@@ -12,18 +12,42 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var nav: UINavigationItem!
     
+    @IBOutlet weak var usersCollectionView : UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-      //  navigationController?.setNavigationBarHidden(true, animated: true)
         
         if var IDs = UserDefaults.standard.object(forKey: "usersIDs") as? [String] {
             print(IDs)
         }
+        
+        usersCollectionView.dataSource = self
+        usersCollectionView.delegate = self
+        
     }
 
 
 }
 
+
+
+extension ViewController : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = usersCollectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+//        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let dimentions = usersCollectionView.frame.size.height
+        return CGSize(width: dimentions, height: dimentions)
+    }
+    
+
+}
